9eb734b4d48dfb533594c6ccf1a0d4c411c262d4
Володя <72095217+VolodjaViv@users.noreply.github.com>
2021-01-29 04:11:11 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <vector>
 #include <mutex>
@@ -12,132 +10,99 @@ using namespace std;
 
 //----------------task_1------------------------------------------------------------------------------------------------
 
-// базовый класс для счетчиков
+const int size_of_byte_array = 1024 * 1024;
+const int threads[] = { 4, 8, 16, 32 };
+
 class Counter {
-public:
-    Counter(const int &dataSize) {
-        _size = dataSize;
-        _id = 0;
-    }
-
-    bool canContinue() {
-        return _id < _size;
-    }
-
-    void reset() {
-        _id = 0;
-    }
-
-    virtual int next() = 0; // абстрактный метод для следующего необработанного индекса
 protected:
     int _id;
     int _size;
+public:
+    Counter(const int& size_of_byte_array) { _id = 0; _size = size_of_byte_array; }
+    void set_zero_id() { _id = 0; }
+    bool check_for_continuation() { return _id < _size; }
+    virtual int swap_to_next_id() = 0;
 };
-
-//------------
 
 class Atomic : public Counter {
+private:
+    atomic_bool _busy;
 public:
-    Atomic(const int &dataSize) : Counter(dataSize) {
-        _busy = false;
-    }
-
-    int next() override{
-        int id;
+    Atomic(const int& size_of_byte_array) : Counter(size_of_byte_array) { _busy = false; }
+    int swap_to_next_id() override {
+        int treated_id;
         while (true) {
-            bool expected = false;
-            if (!_busy.compare_exchange_strong(expected, true)) // если _busy == true, то continue иначе меняет busy на true и идет дальше ифа
+            bool is_busy = false;
+            if (!_busy.compare_exchange_strong(is_busy, true))
                 continue;
-            id = _id++; // следующий необработанный индекс будет возвращен а к самому индексу приавиться единица
-            _busy = false; // делает _busy оратно false чтоб следующий поток мог взять необработанный индекс
+            treated_id = _id;
+            ++_id;
+            _busy = false;
             break;
         }
-        return id;
+        return treated_id;
     }
-private:
-
-    std::atomic_bool _busy;
 };
-
-//------------
 
 class Mutex : public Counter {
-public:
-    Mutex(const int &dataSize) : Counter(dataSize) {}
-
-    int next() override{
-        int id;
-        _mtx.lock(); // блокирует все остальные потоки
-        id = _id++;
-        _mtx.unlock(); // разблокирует все потоки
-        return id;
-    }
-
 private:
-    std::mutex _mtx;
+    std::mutex _mutx;
+public:
+    Mutex(const int& size_of_byte_array) : Counter(size_of_byte_array) {}
+    int swap_to_next_id() override { int id; _mutx.lock(); id = _id; _id++; _mutx.unlock(); return id; }
 };
 
-//------------
-const int size1 = 1024 * 1024; // размер массива
-const int threads[] = {4, 8, 16, 32}; // количество потоков
-
-void threadFunction(Counter *counter, int8_t *arr, const bool *sleep) { // функция которую выполняет поток
-    while (counter->canContinue()) { // пока индекс меньше размера массива
-        arr[counter->next()]++; // инкрементирует необработанный элемент
-        if (*sleep) // если нужно усыплять поток
-            std::this_thread::sleep_for(std::chrono::nanoseconds(10)); // усыпляет текущий поток на 10 наносекунд
-    }
+void initialize_array_with_zeros(int8_t* _array_of_bytes) {
+    for (int i = 0; i < size_of_byte_array; i++) _array_of_bytes[i] = 0;
 }
 
-void setZeros(int8_t *arr) {
-    for (int i = 0; i < size1; i++)
-        arr[i] = 0;
-}
-
-bool check(const int8_t *arr) {
-    for (int i = 0; i < size1; i++)
-        if(arr[i] != 1)
-            return false;
+bool result_array_cheking(const int8_t* result_array_of_bytes) {
+    for (int i = 0; i < size_of_byte_array; i++)
+        if (result_array_of_bytes[i] != 1) return false;
     return true;
 }
 
-void run(Counter &counter, int8_t *arr, const int &threadNum, const bool &sleep) {
-    counter.reset();
-    setZeros(arr);
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now(); // время начала выполнения
-    std::thread *threads = new std::thread[threadNum];
-    for (int i = 0; i < threadNum; i++) {
-        threads[i] = std::thread(threadFunction, &counter, arr, &sleep); // инициирует массив потоков потоками с функцией для потоков внутри
+void exucutable_func(Counter* counter, int8_t* _array_of_bytes, const bool* sleep) {
+    while (counter->check_for_continuation()) {
+        _array_of_bytes[counter->swap_to_next_id()]++;
+        if (*sleep) this_thread::sleep_for(chrono::nanoseconds(10));
     }
-    for (int i = 0; i < threadNum; i++) {
-        threads[i].join();  // ждет пока потоки не завершат работу
-    }
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();  // время конца выполнения
-    if (sleep)
-        std::cout << "With sleep:" << std::endl;
-    else
-        std::cout << "Without sleep:" << std::endl;
-    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(
-            end - begin).count()  // расчет времени выполнения
-              << " milliseconds. Check:" << (check(arr) ? "true" : "false")
-              << "\n";  // проверяет массив на равномерное заполнения единицами
 }
 
-void task1() {
-    Mutex mutex(size1); // создает счетчик с мьютексом с массивом размера size внутри
-    Atomic atomic(size1); // создает атомарны счетчик с массивом размера size внутри
-    int8_t *arr = new int8_t[size1];
+void run(Counter& counter, int8_t* array_of_bytes, const int& threadNum, const bool& sleep) {
+    counter.set_zero_id();
+    initialize_array_with_zeros(array_of_bytes);
+
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    thread* threads = new thread[threadNum];
+    for (int i = 0; i < threadNum; i++) { threads[i] = thread(exucutable_func, &counter, array_of_bytes, &sleep); }
+    for (int i = 0; i < threadNum; i++) { threads[i].join(); }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+    cout << "Time of execution: " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " ms" << endl;
+    cout << "Is true result: " << (result_array_cheking(array_of_bytes) ? "true" : "false") << "\n";
+}
+
+void print_theads_result() {
+    int8_t* byte_array = new int8_t[size_of_byte_array];
+    Atomic atomic_counter(size_of_byte_array);
+    Mutex mutex_counter(size_of_byte_array);
     for (int i = 0; i < 4; i++) {
-        std::cout << "================THREADS " << threads[i] << "================" << std::endl;
-        std::cout << "--------Atomic--------" << std::endl;
-        run(atomic, arr, threads[i], false);
-        run(atomic, arr, threads[i], true);
-        std::cout << "--------Mutex--------" << std::endl;
-        run(mutex, arr, threads[i], false);
-        run(mutex, arr, threads[i], true);
+        cout << "--------------------------" << endl;
+        cout << "Number of threads: " << threads[i] << endl;
+        cout << "--------------------------" << endl;
+        cout << "Using atomic: " << endl << endl;
+        run(atomic_counter, byte_array, threads[i], false);
+        cout << endl << "Using thread sleeping: " << endl;
+        run(atomic_counter, byte_array, threads[i], true);
+        cout << "--------------------------";
+        cout << endl << "Using mutex: " << endl << endl;
+        run(mutex_counter, byte_array, threads[i], false);
+        cout << endl << "With sleep:" << endl;
+        run(mutex_counter, byte_array, threads[i], true);
+        cout << endl;
     }
 }
-
 
 //----------------task_2------------------------------------------------------------------------------------------------
 
@@ -147,8 +112,8 @@ static const int NumConsumers = 1;
 static const int QueueSize = 4;
 vector<thread> producers;
 vector<thread> consumers;
-atomic<int> Sum {0};
-atomic<int> Counter {0};
+atomic<int> Sum{ 0 };
+atomic<int> Counter{ 0 };
 
 template <class T>
 void Consumer(T& q) {
@@ -177,7 +142,7 @@ public:
     void Push(uint8_t val) {
         unique_lock<mutex> uLocker(mutx);
         // поток становится в ожидание, если предикат ложен
-        pushConditionVariable.wait(uLocker, [this] {return !isFull(); } );
+        pushConditionVariable.wait(uLocker, [this] {return !isFull(); });
         queue.push(val);
         popConditionVariable.notify_all();
     }
@@ -195,10 +160,10 @@ private:
     mutex mutx;
 public:
     void Push(uint8_t value) { unique_lock<mutex> uLocker(mutx); queue.push(value); }
-    bool Pop(uint8_t& val) {
+    bool Pop(uint8_t& value) {
         unique_lock<mutex> uLocker(mutx);
         if (queue.empty()) { uLocker.unlock(); this_thread::sleep_for(chrono::milliseconds(1)); uLocker.lock(); }
-        if (!queue.empty()) { val = queue.front(); queue.pop(); return true; }
+        if (!queue.empty()) { value = queue.front(); queue.pop(); return true; }
         else { return false; }
     }
 } di;
@@ -217,10 +182,10 @@ void count_static_sum() {
     cout << "Result by static: " << Sum << endl;
 
     cout << "Sum equals: ";
-    if(Sum == NumProducers * NumTasks) cout << "true" << endl;
+    if (Sum == NumProducers * NumTasks) cout << "true" << endl;
     else cout << "false" << endl;
 
-    cout << "Time check: " << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
+    cout << "Time of execution: " << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
 
     consumers.clear();
     producers.clear();
@@ -240,27 +205,26 @@ void count_dinamic_sum() {
     cout << "Result by dinamic: " << Sum << endl;
 
     cout << "Sum equals: ";
-    if(Sum == NumProducers * NumTasks) cout << "true" << endl;
+    if (Sum == NumProducers * NumTasks) cout << "true" << endl;
     else cout << "false" << endl;
 
-    cout << "Time check: " << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
+    cout << "Time of execution: " << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
 
     consumers.clear();
     producers.clear();
 }
 //----------------------------------------------------------------------------------------------------------------------
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
     //------task_1------
-    cout << "----------Task_1----------" << endl;
-    task1();
-    cout << endl;
+    cout << "----------Task_1----------" << endl << endl;
+    print_theads_result();
     //------task_2------
-    cout << "----------Task_2----------" << endl;
+    cout << "----------Task_2----------" << endl << endl;
     count_static_sum();
     cout << endl;
     count_dinamic_sum();
-    cout << "--------------------------" << endl;
+    cout << endl << "--------------------------" << endl;
     //-----------------
     return 0;
 }
